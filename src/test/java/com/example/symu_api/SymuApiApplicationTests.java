c4f0695f4f38d9b02b56983d27ef4ce3a846ee11
Isaac <isaac.owalo@gmail.com>
2024-07-09 00:30:16 +0300

    first commit

---
+++ src/test/java/com/example/symu_api/SymuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.symu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
